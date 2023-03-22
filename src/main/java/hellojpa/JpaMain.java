dc1b2cfa3c9d99fc28a473faa959ad1fc1068663
Ujoonnee <dnwns0933@naver.com>
2023-03-22 19:35:30 +0900

    ~ 영속성 관리 - 내부 동작 방식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,16 +15,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 영속
-            Member member = em.find(Member.class, 1L);      // 1차 캐시에 없으므로 DB에서 SELECT
-            member.setName("memberDetached");                   // em.clear()에 의해 UPDATE 쿼리를 생성하지 않음
+            Member member = new Member();
+            member.setName("memberB");
+            em.persist(member);
 
-            em.clear();                                         // 영속성 컨텍스트 초기화
-
-            Member member1 = em.find(Member.class, 1L);     // 초기화에 의해 1차 캐시에 없으므로 다시 DB에서 SELECT
-            member1.setName("memberAAA");                       // flush 시점에 UPDATE 쿼리 생성
-
-            tx.commit();                                        // commit 전 자동으로 flush
+            tx.commit();
 
         } catch (Exception e) {
             tx.rollback();
